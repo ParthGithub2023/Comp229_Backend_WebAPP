c8810952fcdd4571d69ef2b023db357852f38755
Parth Savjani <parthsavjani01@gmail.com>
2023-10-15 22:37:40 -0400

    first commit

---
+++ config/config.js
@@ -0,0 +1,4 @@
+// Alert: For production environment never expose your connection string AND secret keys.
+module.exports = {
+    "ATLASDB" : "mongodb+srv://parthsavjani01:ParthApi@mongodb.a9tvofd.mongodb.net/Marketplace?retryWrites=true&w=majority"
+}
